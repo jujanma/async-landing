7d1f017a418386e7aa5ee08921e211f36043f5cc
Juan Manuel Marin <juanchomanuel289@gmail.com>
2024-08-16 16:11:19 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,10 @@
-const API =
-  "https://youtube-v31.p.rapidapi.com/search?relatedToVideoId=7ghhRHRP6t4&part=id%2Csnippet&type=video&maxResults=50";
+const API = "https://rickandmortyapi.com/api/character";
 
 const content = null || document.getElementById("content");
 const options = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "09b48c25e6mshf6bbc1ec8178732p1b1ac1jsnceefd165a8d2",
-    "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+    "Content-Type": "application/json",
   },
 };
 
@@ -18,22 +16,22 @@ const fetchData = async (url) => {
 
 (async () => {
   try {
-    const videos = await fetchData(API);
-    console.log(videos.items);
+    const characters = await fetchData(API);
+    console.log(characters.results);
     let view = `
-    ${videos.items
+    ${characters.results
       .map(
-        (video) => `
+        (character) => `
          <div class="group relative">
             <div
               class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
             >
-              <img src="${video?.snippet?.thumbnails?.high?.url}" alt="${video?.snippet?.description}" class="w-full" />
+              <img src="${character?.image}" alt="${character?.name}" class="w-full" />
             </div>
             <div class="mt-4 flex justify-between">
               <h3 class="text-sm text-gray-700">
                 <span aria-hidden="true" class="absolute inset-0"></span>
-                    ${video?.snippet?.title}
+                    ${character?.name}
               </h3>
             </div>
           </div>
